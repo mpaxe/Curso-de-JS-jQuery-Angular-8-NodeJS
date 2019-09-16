857b17b859528b171cb4d39ce4822b848a4f1fbc
mpaxe <mpaxe@hotmail.com>
2019-09-16 17:30:00 +0200

    Nuevos ejercicios con promesas (fetch)

--- js/36-fetch.js
+++ js/36-fetch.js
@@ -1,74 +1,25 @@
 'use strict'
 
-// Fetch (ajax) y peticiones a servicios / apis rest
-
-var espacios = '&nbsp;&nbsp;&nbsp;&nbsp;'
+// Fetch (ajax) y peticiones a servicios / api rest
 
 var div_usuarios = document.querySelector('#usuarios');
 
-// Otra forma de hacerlo
 
-// fetch('https://reqres.in/api/users')
-//     .then(function(data){
-//         return data.json()
-//     })
-//     .then(users => {
-//         usuarios = users.data;
-//         console.log(usuarios);
-//     });
-
-getUsuarios()
+fetch('https://reqres.in/api/users')   
     .then(data => data.json())
     .then(users => {
-        listadoUsuarios(users);
-
-        return getUsuario(3);
-    })
-    .then(data => data.json())
-    .then(user => {
-        mostrarUsuario(user);
+        listadoUsuarios(users.data);
+        
     });
-
-function getUsuarios(){
-    return fetch('https://jsonplaceholder.typicode.com/users');
-}
-
-function getUsuario(id = 1){
-    return fetch('https://jsonplaceholder.typicode.com/users/'+id);
-}
 
 function listadoUsuarios(usuarios){
     usuarios.map((user, i) => {
-        let nombre = document.createElement('p');
+        let nombre = document.createElement('h3');
 
-        if(user.id < 10){
-            nombre.innerHTML = user.id+") <b>Nombre:</b> "+user.name+"<br /><b>"+
-            espacios+"Usuario:</b> &nbsp;"+user.username;
-        }else if(user.id < 100){
-            nombre.innerHTML = user.id+") <b>Nombre:</b> "+user.name+"<br /><b>"+
-            espacios+"&nbsp;&nbsp;Usuario:</b> &nbsp;"+user.username;
-        }
-        
+        nombre.innerHTML = i + '. '+user.first_name +' '+user.last_name;
+
         div_usuarios.appendChild(nombre);
 
-        document.querySelector(".loading").style.display = 'none';
+        document.querySelector('.loading').style.display = 'none';
     });
 }
-
-function mostrarUsuario(usuario){
-    let nombre = document.createElement('p');
-    let separacion = document.createElement('hr');
-    let encabezado = document.createElement('h1');
-
-    nombre.innerHTML = usuario.id+") <b>Nombre:</b> "+usuario.name+"<br /><b>"+
-    espacios+"Usuario:</b> &nbsp;"+usuario.username;
-    encabezado.innerHTML = "<h4>Mostrar usuario</h4>";
-   
-    div_usuarios.appendChild(separacion);
-    div_usuarios.appendChild(encabezado);
-    div_usuarios.appendChild(nombre);
-
-    document.querySelector(".loading_dos").style.display = 'none';
-}
-
- 
